0def6a4a040f2808e0f6b1af84200e8f7ec01453
joseniquen08 <j.armando0807@gmail.com>
2021-08-17 01:21:51 -0500

    Avance react

--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -118,7 +118,7 @@ const Informacion = () => {
                           </div>
                           <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                              Talleres
+                              Mini talleres
                             </dt>
                             <dd className="mt-1 text-base text-gray-900 dark:text-white sm:mt-0 sm:col-span-2">
                               {detalles.talleres}
